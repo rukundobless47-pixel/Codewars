521c6cd39f0cae49bd9345685819fcf5e6d40aee
Bless <rukundobless47@gmail.com>
2026-07-07 10:39:16 +0200

    Add the given number and the if sum length greater than 1 sum it again

---
+++ 6 kyu Sum of Digits Digital Root.js
@@ -0,0 +1,12 @@
+function digitalRoot(n) {
+    let sum=n;
+  while(sum>9){
+  let arr = sum.toString().split('').map(Number);
+  sum=0;
+  for(let i=0; i<arr.length; i++){
+          sum += arr[i]; 
+  }
+}
+  return sum;
+}
+console.log(digitalRoot(493193));
